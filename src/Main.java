2780f8dc9a5bcf6b6de65b24968621b5d9350133
Артем <artemgolyshkov@mail.ru>
2023-02-15 21:48:56 +0300

    Домашняя работа № 3 задача 1.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        int a = 4000;
+        System.out.println("Значение переменной а с типом int равно " + a);
+        byte b = 122;
+        System.out.println("Значение переменной b с типом byte равно " + b);
+        short bi = 15236;
+        System.out.println("Значение переменной bi с типом short равно " + bi);
+        long d = 10237586;
+        System.out.println("Значение переменной d с типом long равно " + d);
+        float e = -5.75f;
+        System.out.println("Значение переменной d с типом float равно " + e);
+        double v = 5.75;
+        System.out.println("Значение переменной v с типом double onравно " + v);
     }
 }
